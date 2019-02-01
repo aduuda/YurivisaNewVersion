ae4c709adf6667ab7541d08f2d81ba3c18342070
aduuda <45482016+aduuda@users.noreply.github.com>
2019-02-01 12:49:26 +0700

    up website yuri mới lần 1

---
+++ YuriVisaNewVersion/Content/Site/Site.js
@@ -0,0 +1,69 @@
+﻿//$('.owl-carousel').owlCarousel({
+//    loop: true,
+//    margin: 10,
+//    nav: true,
+//    responsive: {
+//        0: {
+//            items: 1
+//        },
+//        600: {
+//            items: 3
+//        },
+//        1000: {
+//            items: 5
+//        }
+//    }
+//})
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 50) {
+            $("#MainNav").css("background-color", "rgba(254, 249, 250,0.7)");
+            console.log(scroll);
+        }
+    });
+});
+$('#gallery').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: [
+        "<i class='fas fa-arrow-left'></i>",
+        "<i class='fas fa-arrow-right'></i>"
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+$('#customer').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: [
+        "<i class='fas fa-arrow-left'></i>",
+        "<i class='fas fa-arrow-right'></i>"
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
